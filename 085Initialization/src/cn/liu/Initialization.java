a26e5a3fd3d062cd8f7ea765d4b1a9fe82d02c90
lttxiaocao <1536513203@qq.com>
2016-12-11 20:42:11 +0800

    085Initialization

---
+++ 085Initialization/src/cn/liu/Initialization.java
@@ -0,0 +1,28 @@
+package cn.liu;
+
+public class Initialization {
+	private byte b;
+	private short s;
+	private int i;
+	private long l;
+	private float f;
+	private double d;
+	private boolean bl;
+	private char c;
+	private String string;
+
+	public static void main(String[] args) {
+		Initialization init = new Initialization();
+		System.out.println(init.b);
+		System.out.println(init.s);
+		System.out.println(init.i);
+		System.out.println(init.l);
+		System.out.println(init.f);
+		System.out.println(init.d);
+		System.out.println(init.bl);
+		System.out.println(init.c);
+		System.out.println(init.string);
+
+	}
+
+}
